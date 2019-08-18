7d5388fddc73e46afd8e2258f95e58a7bcc62eea
Jeong Park <wjdtn1220@gmail.com>
2019-08-18 19:15:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,5 +10,11 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def
-
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if age <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    
+    end
+  end
+end
